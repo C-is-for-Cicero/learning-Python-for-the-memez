0f32c8708d66289f4052c5847a6310e7918177a5
David Myburgh <61410517+C-is-for-Cicero@users.noreply.github.com>
2020-07-24 11:46:05 +0200

    Add files via upload

---
+++ Section6/CodingExcercise28.py
@@ -0,0 +1,4 @@
+def greeting_function(name):
+    # greeting = f"Hi {name}"
+    greeting ="Hi %s " % name
+    return greeting
---
+++ Section6/CodingExcercise29.py
@@ -0,0 +1,4 @@
+def greeting_function(name):
+    name=name.capitalize()
+    greeting ="Hi %s " % name
+    return greeting
